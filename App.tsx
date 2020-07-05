2c593917c3fe5d73ef097fee7d2e0ffaf973eb8f
Sanmugam Kathirvel <k.sanmugam2@gmail.com>
2020-07-05 12:03:41 +0530

    hardwareBackPress back press event handler

--- App.tsx
+++ App.tsx
@@ -1,10 +1,71 @@
-import React from 'react';
-import { WebView } from 'react-native-webview';
+import React, { useState, useRef, useEffect } from "react";
+import {
+  SafeAreaView,
+  StyleSheet,
+  StatusBar,
+  ActivityIndicator,
+  BackHandler,
+  Platform,
+} from "react-native";
+import WebView from "react-native-webview";
 
-export default function App() {
+const App = () => {
+
+  const [canGoBack, setCanGoBack] = useState(false);
+
+  const webviewRef = useRef(null);
+
+  backButtonHandler = () => {
+    if (canGoBack) webviewRef.current.goBack();
+    else BackHandler.exitApp();
+  };
+
+  useEffect(() => {
+    const backAction = () => {
+      backButtonHandler();
+      return true;
+    };
+
+    // This feature will work only in Android
+    if (Platform.OS === 'android' ) {
+        const backHandler = BackHandler.addEventListener(
+          "hardwareBackPress",
+          backAction
+        );
+
+        return () => backHandler.remove();
+    }
+
+  }, []);
+
   return (
-      <WebView
-        source={{ uri: 'http://jotiksha.com' }}
-      />
-  )
-}
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView style={styles.flexContainer}>
+        <WebView
+          source={{ uri: "http://jotiksha.com/" }}
+          startInLoadingState={true}
+          renderLoading={() => (
+            <ActivityIndicator
+              color="black"
+              size="large"
+              style={styles.flexContainer}
+            />
+          )}
+          ref={webviewRef}
+          onNavigationStateChange={(navState) => {
+            setCanGoBack(navState.canGoBack);
+          }}
+        />
+      </SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  flexContainer: {
+    flex: 1,
+  },
+});
+
+export default App;
